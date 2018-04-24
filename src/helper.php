5ff59bb9b058a5bb27a755d475a2085812af25d4
king_east_gs <1207877378@qq.com>
2018-04-24 15:45:09 +0800

    Signed-off-by: king_east_gs <1207877378@qq.com>

--- src/helper.php
+++ src/helper.php
@@ -152,3 +152,43 @@ if (!function_exists('get_children_list')) {
         return $arr;
     }
 }
+
+
+
+
+if (!function_exists('get_distance')) {
+    /**
+     * 计算两点地理坐标之间的距离
+     * @param  float $longitude1 起点经度
+     * @param  float $latitude1  起点纬度
+     * @param  float $longitude2 终点经度
+     * @param  float $latitude2  终点纬度
+     * @param  integer     $unit       单位 1:米 2:公里
+     * @param  integer     $decimal    精度 保留小数位数
+     * @return float
+     */
+    function get_distance($longitude1, $latitude1, $longitude2, $latitude2, $unit=2, $decimal=2){
+
+        $EARTH_RADIUS = 6370.996; // 地球半径系数
+        $PI = 3.1415926;
+
+        $radLat1 = $latitude1 * $PI / 180.0;
+        $radLat2 = $latitude2 * $PI / 180.0;
+
+        $radLng1 = $longitude1 * $PI / 180.0;
+        $radLng2 = $longitude2 * $PI /180.0;
+
+        $a = $radLat1 - $radLat2;
+        $b = $radLng1 - $radLng2;
+
+        $distance = 2 * asin(sqrt(pow(sin($a/2),2) + cos($radLat1) * cos($radLat2) * pow(sin($b/2),2)));
+        $distance = $distance * $EARTH_RADIUS * 1000;
+
+        if($unit==2){
+            $distance = $distance / 1000;
+        }
+
+        return round($distance, $decimal);
+
+    }
+}
